35e4dd0a27bb4893c317247316d79d77bd7fdfc1
animanathome <animanatwork@me.com>
2023-06-05 07:38:29 -0700

    cleanup

--- tests/resources.spec.ts
+++ tests/resources.spec.ts
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 describe('resources', () => {
-   it('can be loaded', async() => {
+    it('can be loaded', async() => {
         const response = await fetch('http://localhost:8080/resources/Montserrat-Regular.ttf');
         expect(response.status).to.equal(200);
-   });
+    });
 });
