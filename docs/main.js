b732d07b4b802e1a8009208c40b43e241ae78acf
animanathome <animanatwork@me.com>
2023-06-27 09:14:21 -0700

    testing

--- docs/main.js
+++ docs/main.js
@@ -67145,7 +67145,7 @@ var buildTextCanvas = function (text) { return __awaiter(void 0, void 0, void 0,
                 app.view.style.height = "".concat(height, "px");
                 app.view.style.width = "".concat(width, "px");
                 fontLoader = new _src_fontLoader__WEBPACK_IMPORTED_MODULE_1__.FontLoader();
-                fontUrl = 'https://animanathome.github.io/pixi-text-editing/Roboto-Regular.ttf';
+                fontUrl = './Roboto-Regular.ttf';
                 console.log('fontUrl', fontUrl);
                 fontLoader.sourceUrl = fontUrl;
                 return [4 /*yield*/, fontLoader.load()];
